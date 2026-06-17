badf78bfc57674cb9959818d9b25d64872c71449
carloshsantos1608 <carlos.hsantos@ufrpe.br>
2026-06-17 14:18:25 -0300

    Update buscas.py

--- mecanicas/buscas.py
+++ mecanicas/buscas.py
@@ -11,14 +11,9 @@ class GerenciadorBuscas:
         """Custa tempo e energia. Retorna se o item foi spawnado com sucesso"""
         jogador.passar_tempo(self.custo_tempo)
         jogador.modificar_energia(self.custo_energia)
-        
-        # Se o jogador já pegou esse item antes, não spawna de novo
         if item_do_local in jogador.itens_encontrados:
             return "Você revirou tudo, mas lembra que já pegou o que precisava aqui.", False
-            
-        # Chance de o item aparecer na tela para o jogador clicar
         if random.random() < self.chance_sucesso:
             msg = "Você examinou o local... Parece que tem algo por aqui. Clique nele!"
-            return msg, True
-            
+            return msg, True     
         return "Você procurou atrás das cadeiras e armários, mas não achou nada útil.", False
