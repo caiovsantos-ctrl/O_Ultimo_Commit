77980941b4232e7ea24f9fef85bd8219341d1ff0
carloshsantos1608 <carlos.hsantos@ufrpe.br>
2026-06-17 14:24:31 -0300

    Update sobrevivencia.py

--- mecanicas/sobrevivencia.py
+++ mecanicas/sobrevivencia.py
@@ -8,17 +8,13 @@ class GerenciadorSobrevivencia:
     def comprar_comida(self, jogador, tipo: str) -> tuple[str, bool]:
         """Processa a compra de comida baseada no cardápio"""
         if tipo not in self.cardapio:
-            return "Tipo de comida inválido.", False
-            
-        info = self.cardapio[tipo]
-        
+            return "Tipo de comida inválido.", False          
+        info = self.cardapio[tipo]        
         if jogador.dinheiro < info["preco"]:
             if tipo == "RU":
                 return "Sem dinheiro até para o RU! A humilhação é real.", False
-            return "Dinheiro insuficiente para a lanchonete! Esse salgado gourmet não é para o seu bico.", False
-            
+            return "Dinheiro insuficiente para a lanchonete! Esse salgado gourmet não é para o seu bico.", False           
         jogador.modificar_dinheiro(-info["preco"])
         jogador.passar_tempo(info["tempo"])
-        jogador.modificar_energia(info["energia"])
-        
+        jogador.modificar_energia(info["energia"])       
         return info["sucesso_msg"], True
