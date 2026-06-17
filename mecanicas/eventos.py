a89269f15b75bf77defb626a7cb5b7cf7307c39b
carloshsantos1608 <carlos.hsantos@ufrpe.br>
2026-06-17 14:19:43 -0300

    Update eventos.py

--- mecanicas/eventos.py
+++ mecanicas/eventos.py
@@ -9,8 +9,7 @@ class GerenciadorEventos:
         """Aplica o resultado da decisão com o Saguim bloqueando o caminho"""
         if escolha == "tempo":
             jogador.passar_tempo(self.penalidade_tempo_saguim)
-            return "Você perdeu um bom tempo fazendo caminhos alternativos e despistando o saguim. (-15⏰)"
-        
+            return "Você perdeu um bom tempo fazendo caminhos alternativos e despistando o saguim. (-15⏰)"     
         jogador.modificar_energia(self.penalidade_energia_saguim)
         return "Você correu com tudo! O saguim pulou na sua direção, te deu um susto e você ficou exausto. (-8⚡)"
 
@@ -18,7 +17,6 @@ class GerenciadorEventos:
         """Aplica o resultado da decisão com o Veterano tagarela no corredor"""
         if escolha == "ouvir":
             jogador.passar_tempo(self.penalidade_tempo_veterano)
-            return "Ele falou por 20 minutos seguidos sem respirar... Pelo menos você descobriu que não cai Fluxogramas na prova! (-20⏰)"
-        
+            return "Ele falou por 20 minutos seguidos sem respirar... Pelo menos você descobriu que não cai Fluxogramas na prova! (-20⏰)"      
         jogador.modificar_energia(self.penalidade_energia_veterano)
         return "Você se afastou. Ele gritou 'VOCÊ VAI REPROVAR!', o que te deixou muito estressado... (-5⚡)"
